900064964879a410419aa8fd7d703573d96b9be2
Coder Juhi <juhichin505@gmail.com>
2023-06-26 00:09:41 -0400

    created bubble chart

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -7,18 +7,23 @@ console.log("testing")
 URL https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json.
 */
 
+//Create variable to store samples.json endpoint stored on Amazon server s3
+const sample_data = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+
+//Fetch the JSON data and console log it
+d3.json(sample_data).then(function(data) {
+    // console.log("This is sample data")
+    console.log(data)
+})
 
 
-
-/* 2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
+/* Build function called buildCharts
+2. Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual.
 Use sample_values as the values for the bar chart.
 Use otu_ids as the labels for the bar chart.
 Use otu_labels as the hovertext for the chart.
-*/
 
-
-
-/* 3. Create a bubble chart that displays each sample.
+3. Create a bubble chart that displays each sample.
 Use otu_ids for the x values.
 Use sample_values for the y values.
 Use sample_values for the marker size.
@@ -26,7 +31,67 @@ Use otu_ids for the marker colors.
 Use otu_labels for the text values.
 */
 
+function buildCharts(sample){
+    console.log("testing buildcharts functions");
 
+    d3.json(sample_data).then(function(data){
+        console.log("This is sample data")
+        console.log(data);
+
+        //to build the barchart need the samples object
+        let samples = data.samples
+        //view in console
+        console.log(samples);
+
+        let sampleArray = samples.filter(sampleObject => sampleObject.id == sample);
+        console.log(sampleArray);
+
+        //unpack the object from within the array
+        let sampleResult = sampleArray[0];
+        console.log(sampleResult);
+
+        //create variables 
+        let sample_values = sampleResult.sample_values;
+        console.log(sample_values);
+
+        let otu_ids = sampleResult.otu_ids;
+        console.log(otu_ids);
+
+        let otu_labels = sampleResult.otu_labels;
+        console.log(otu_labels);
+
+        /* Bubble chart: 
+                Use otu_ids for the x values.
+                Use sample_values for the y values.
+                Use sample_values for the marker size.
+                Use otu_ids for the marker colors.
+                Use otu_labels for the text values. */
+        
+                let traceBubble = {
+                    x: otu_ids,
+                    y: sample_values, 
+                    mode: 'markers',
+                    marker: {
+                        size: sample_values,
+                        color: otu_ids,
+                        colorscale: 'YlGnBu'
+                    }
+                }
+
+                let dataBubble = [traceBubble];
+
+                let layoutBubble = {
+                    title: "Bacteria in Sample",
+                    showlegend: false,
+                    xaxis: {title: "OTU ID"},
+                    yaxis: {title: "Sample Values"}
+                }
+
+                //build bubble chart in <div id="bubble"></div>
+                Plotly.newPlot('bubble', dataBubble, layoutBubble)
+    });
+
+};
 
 
 /* 4. Display the sample metadata, i.e., an individual's demographic information. */
@@ -48,3 +113,10 @@ create any layout that you would like for your dashboard. */
 Submit the links to your deployment and your GitHub repo. 
 Ensure that your repository has regular commits and a thorough README.md file */
 
+
+//Create an initialize function called initialize
+function initialize(){
+    buildCharts(940);
+};
+
+initialize();
